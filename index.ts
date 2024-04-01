9041268c17a54d87162133d2623487b2209bf2b3
FAIZA KANWAL <faizakanwal5050@gmail.com>
2024-04-01 17:11:33 +0500

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const randomNumber = Math.floor(Math.random() * 10+1);
+
+const answers = await inquirer.prompt([
+    {
+name: "userGuessNumber",
+type: "number",
+message: "Please guess a number between 1-10: ",
+    }
+]);
+
+if(answers.userGuessNumber === randomNumber ){
+    console.log("Congratulation! you guessed a right number.");
+}else {
+    console.log("Oh! You guessed wrong number");
+}
